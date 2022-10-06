880575e4cf620a11249d12f0726130ddd5369b3f
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2022-10-06 22:34:03 +0700

    Update App.js

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Helmet } from 'react-helmet';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
@@ -11,11 +10,6 @@ import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Helmet>
-        <title>Portfolio - KIKI</title>
-      </Helmet>
-        <div>
     <div className="App">
       <NavBar />
       <Banner />
