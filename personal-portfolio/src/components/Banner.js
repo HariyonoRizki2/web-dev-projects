92e1ddc27edc55ca302beb1db22760e330c7c36c
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2022-10-06 22:55:29 +0700

    Update Banner.js

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -11,8 +11,8 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  <!-- const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ]; -->
-  <!-- const period = 2000; -->
+  const toRotate = [ "Fotografer", "Videografer", "Editor" ];
+  const period = 2000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -55,8 +55,8 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <!-- <span className="tagline"></span> -->
-                <h1>{`Hai! Aku Hariyono Rizki`}</h1>
+                <span className="tagline">Selamat Datang!</span>
+                <h1>{`Hai! Aku Hariyono Rizki`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Fotografer", "Videografer", "Editor" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Kontak <ArrowRightCircle size={25} /></button>
               </div>}
