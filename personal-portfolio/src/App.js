d447f29bbddc02bdba8bce802ba65570c04c0d22
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2022-10-06 22:24:54 +0700

    Update App.js

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from 'react-helmet';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { Helmet } from 'react-helmet';
 
 function App() {
   return (
