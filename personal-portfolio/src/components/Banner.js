2c75d9e118ccb1d96bc5af713347e676e426093f
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2022-10-06 23:04:35 +0700

    Update Banner.js

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { ArrowDownCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = [ "Fotografer", "Videografer", "Editor" ];
-  const period = 2000;
+  const period = 5000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -56,7 +56,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Selamat Datang!</span>
-                <h1>{`Hai! Aku KIKI.`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Fotografer", "Videografer", "Editor" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hai! Aku KIKI.`} <span className="txt-rotate" dataPeriod="2000" data-rotate='[ "Fotografer", "Videografer", "Editor" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick="window.location.href='./cv.pdf'">Kontak <ArrowDownCircle size={25} /></button>
               </div>}
